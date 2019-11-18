6c9d81dba7c04df51db20fe708896b04f42c0ce7
Lordph8 <johnwhammer@gmail.com>
2019-11-18 16:52:08 +0000

    added readme + script edit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 function dice() {
-    var tDice = document.getElementById('type').value;  
-    if (tDice == 'd2') { var num = Math.ceil((Math.random()*10)*0.2);}
-    if (tDice == 'd4') {var num = Math.ceil((Math.random()*10)*0.4);}
-    if (tDice == 'd6') {var num = Math.ceil((Math.random()*10)*0.6);}
-    if (tDice == 'd8') {var num = Math.ceil((Math.random()*10)*0.8);}
-    if (tDice == 'd10') {var num = Math.ceil((Math.random()*10));}
-    if (tDice == 'd12') {var num = Math.ceil((Math.random()*10)*1.2);}
-    if (tDice == 'd20') {var num = Math.ceil((Math.random()*20));}
-    if (tDice == 'd100') {var num = Math.ceil((Math.random()*100));}
-    return num;
+    var tDice = document.getElementById('type').value;
+    var x;
+    if (tDice == 'd2') { x = 0.2 ;}
+    if (tDice == 'd4') { x = 0.4 ;}
+    if (tDice == 'd6') { x = 0.6 ;}
+    if (tDice == 'd8') { x = 0.8 ;}
+    if (tDice == 'd10') { x = 1 ;}
+    if (tDice == 'd12') { x = 1.2 ;}
+    if (tDice == 'd20') { x = 2 ;}
+    if (tDice == 'd100') { x = 10 ;}
+    var num = Math.ceil(Math.random()*10*x);
+    return num
 }
 
 function roll() {
@@ -31,58 +33,61 @@ function roll() {
     if (tDice == 'd20') {var max = 20;}
     if (tDice == 'd100') {var max = 100;}
 
-    for (i = 0; i < nDice; i++) {
-        var number = dice();
-        log.push(number); 
-        if (tDice == 'd100') {
-            $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 100px; height: 100px;" />')
-        } else {
-            $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 50px; height: 50px;" />')
+    if (nDice > "" && tDice > "") {
+
+        for (i = 0; i < nDice; i++) {
+            var number = dice();
+            log.push(number); 
+            if (tDice == 'd100') {
+                $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 100px; height: 100px;" />')
+            } else {
+                $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 50px; height: 50px;" />')
+            }
+            if (special == 'Dice Explosion!' && number == max) {
+                while (number == max) {
+                    var number = dice();
+                    log.push(number); 
+                    if (tDice == 'd100') {
+                        $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 100px; height: 100px;" />')
+                    } else {
+                        $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 50px; height: 50px;" />')
+                    }
+                }
+            } 
         }
-        if (special == 'Dice Explosion!' && number == max) {
-            while (number == max) {
-                var number = dice();
-                log.push(number); 
-                if (tDice == 'd100') {
-                    $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 100px; height: 100px;" />')
+
+        if (special == 'Get Max Roll' || special == 'Get Min Roll') {
+            if (special == 'Get Min Roll') {
+
+                var disAdvantage = Number(Math.min(...log)) + bonus; 
+                            
+                if (bonus != 0 || bonus != "") {
+                    $('.pictures').append('<p class= "total">+ Bonus(' + bonus + ') = ' + disAdvantage + '</p>');
                 } else {
-                    $('.pictures').append('<img id= "picture" src= ' + folder + '/' + number +'.svg style= "width: 50px; height: 50px;" />')
+                    $('.pictures').append('<p class= "total"> = ' + disAdvantage + '</p>');
+                }
+                
+            } else {   
+
+                var advantage = Number(Math.max(...log)) + bonus; 
+                
+                if (bonus != 0 || bonus != "") {
+                    $('.pictures').append('<p class= "total">+ Bonus(' + bonus + ') = ' + advantage + '</p>');
+                } else {
+                    $('.pictures').append('<p class= "total"> = ' + advantage + '</p>');
                 }
             }
-        } 
-    }
-
-    if (special == 'Get Max Roll' || special == 'Get Min Roll') {
-        if (special == 'Get Min Roll') {
-
-            var disAdvantage = Number(Math.min(...log)) + bonus; 
-                        
-            if (bonus != 0 || bonus != "") {
-                $('.pictures').append('<p class= "total">+ Bonus(' + bonus + ') = ' + disAdvantage + '</p>');
-            } else {
-                $('.pictures').append('<p class= "total"> = ' + disAdvantage + '</p>');
+        } else {  
+            for (x = 0; x < log.length; x++) {
+                    var sum = sum + log[x];
             }
-            
-        } else {   
+            var sum = sum + bonus
 
-            var advantage = Number(Math.max(...log)) + bonus; 
-            
             if (bonus != 0 || bonus != "") {
-                $('.pictures').append('<p class= "total">+ Bonus(' + bonus + ') = ' + advantage + '</p>');
+                $('.pictures').append('<p class= "total">+ Bonus(' + bonus + ') = ' + sum + '</p>');
             } else {
-                $('.pictures').append('<p class= "total"> = ' + advantage + '</p>');
+                $('.pictures').append('<p class= "total"> = ' + sum + '</p>');
             }
-        }
-    } else {  
-        for (x = 0; x < log.length; x++) {
-                var sum = sum + log[x];
-        }
-        var sum = sum + bonus
-
-        if (bonus != 0 || bonus != "") {
-            $('.pictures').append('<p class= "total">+ Bonus(' + bonus + ') = ' + sum + '</p>');
-        } else {
-            $('.pictures').append('<p class= "total"> = ' + sum + '</p>');
-        }
+        }    
     }    
 }
